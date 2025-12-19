57101fe6ce33422627f8a4cde535bdefce9bd063
Manav Shah <manav.march12@gmail.com>
2025-12-19 14:01:03 +0530

    fix: Update RmpApplication to use ConfigurationPropertiesScan

--- backend/src/main/java/com/rmp/RmpApplication.java
+++ backend/src/main/java/com/rmp/RmpApplication.java
@@ -1,14 +1,13 @@
 package com.rmp;
 
-import com.rmp.config.ZohoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties(ZohoConfig.class)
+@ConfigurationPropertiesScan
 public class RmpApplication {
 
     public static void main(String[] args) {
